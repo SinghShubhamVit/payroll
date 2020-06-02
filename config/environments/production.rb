0ec2ef2a91cb1a62bf566ad3b62d75651bbfeab8
Bacancy-ShubhamSingh <shubhbitvit95@gmail.com>
2020-06-02 22:52:30 +0530

    added password to credenetial file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: Rails.application.credentials.config[:email],
-    password: Rails.application.credentials.config[:email],
+    password: Rails.application.credentials.config[:password],
     authentication: 'plain',
     enable_starttls_auto: true
   }
